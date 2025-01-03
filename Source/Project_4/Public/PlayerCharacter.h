f56317c1c68cb48213e426a5386643eaed71e4a6
dongin03008 <90513152+dongin03008@users.noreply.github.com>
2025-01-03 17:21:52 +0900

    플레이어 움직임 구현 임 모델 투명도
    플레이어 움직임 구현 임 모델 투명도

--- Source/Project_4/Public/PlayerCharacter.h
+++ Source/Project_4/Public/PlayerCharacter.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "InputActionValue.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Camera/CameraComponent.h"
@@ -10,6 +11,26 @@ UCLASS()
 class PROJECT_4_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
+
+private:
+	// IMC
+	UPROPERTY(editanywhere, category = input, meta = (allowprivateaccess = true))
+	class UInputMappingContext* DefaultMappingContext;
+	// IA_Jump
+	UPROPERTY(editanywhere, category = input, meta = (allowprivateaccess = true))
+	class UInputAction* JumpAction;
+	// IA_Look
+	UPROPERTY(editanywhere, category = input, meta = (allowprivateaccess = true))
+	class UInputAction* LookAction;
+	// IA_Move
+	UPROPERTY(editanywhere, category = input, meta = (allowprivateaccess = true))
+	class UInputAction* MoveAction;
+	UPROPERTY(editanywhere, category = input, meta = (allowprivateaccess = true))
+	class UInputAction* SprintAction;
+	UPROPERTY(editanywhere, category = input, meta = (allowprivateaccess = true))
+	float walkSpeed;
+	UPROPERTY(editanywhere, category = input, meta = (allowprivateaccess = true))
+	float sprintSpeed;
 
 public:
 	// Sets default values for this character's properties
@@ -26,17 +47,12 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UFUNCTION()
-	void MoveForward(float AxisValue);
+	void Move(const FInputActionValue& value);
+	void Look(const FInputActionValue& value);
+	void Sprint();
+	void StopSprint();
+	void Jump() override;
 
-	UFUNCTION()
-	void MoveRight(float AxisValue);
-
-	UFUNCTION()
-	void StartJump();
-
-	UFUNCTION()
-	void StopJump();
 
 	UPROPERTY(VisibleAnywhere)
 	UCameraComponent* CHCameraComponent;
